4750dbdfd55aa54d6acb417eae71009ed361093c
Gabriel <75056711+Gabriel-code21@users.noreply.github.com>
2022-02-22 20:14:46 -0800

    Update Main.java

--- ReadingText/src/Main.java
+++ ReadingText/src/Main.java
@@ -9,20 +9,23 @@ public class Main {
         FileReader fr = new FileReader("ProgrammingHistory.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
+
+        // add all lines in the txt file to the ArrayList
         while ((line = br.readLine()) != null) {
             lines.add(line);
         }
-
+        // choose the word that you want to find
         String wordToFind = "the";
 
         ArrayList<Integer> positionsOfWord = findWordIndex(lines, wordToFind);
         System.out.println("Indexes in ArrayList with "+wordToFind+":");
         System.out.println(positionsOfWord);
     }
-
+    // method to find word in ArrayList
     public static ArrayList<Integer> findWordIndex(ArrayList<String> sentenceList, String word) {
         ArrayList<Integer> posList = new ArrayList<>();
         for (int i = 0; i < sentenceList.size(); i++) {
+            // checks to see if the word is in the ArrayList
             if (sentenceList.get(i).indexOf(" "+word+"") > 0) {
                 posList.add(i);
             }
